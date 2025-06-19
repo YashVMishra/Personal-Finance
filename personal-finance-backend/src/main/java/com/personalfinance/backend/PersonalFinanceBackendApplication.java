1a71950100077790bdfe98bc3c75219289b56fbf
Yash Vardhan Mishra <yash.vardhan.mishra@sap.com>
2025-06-19 12:17:39 +0530

    Backend First Commmit

---
+++ personal-finance-backend/src/main/java/com/personalfinance/backend/PersonalFinanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.personalfinance.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceBackendApplication.class, args);
+	}
+
+}
---
+++ personal-finance-backend/src/test/java/com/personalfinance/backend/PersonalFinanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalfinance.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
